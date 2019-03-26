aca7e3f7ac01adbbb01b470cbb1e2177930eddbe
ccoa <omarccoah@gmail.com>
2019-03-26 10:16:10 -0500

    Actualizacion de SSL

---
+++ src/main/java/com/occoa/intercorp/security/AppSecurity.java
@@ -0,0 +1,20 @@
+package com.occoa.intercorp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class AppSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .requiresChannel().anyRequest().requiresSecure()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
